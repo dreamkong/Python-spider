5ed3fe2c83a6e65c41f99336a791345ac14d8cf6
dreamkong <dreamkong0113@gmail.com>
2019-01-10 11:43:10 +0800

    爬取知乎用户信息存入MongoDB

---
+++ zhihu/zhihuuser/zhihuuser/spiders/zhihu.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+import json
+
+import scrapy
+from scrapy import Request
+
+from zhihuuser.items import UserItem
+
+
+class ZhihuSpider(scrapy.Spider):
+    name = 'zhihu'
+    allowed_domains = ['www.zhihu.com']
+    start_urls = ['http://www.zhihu.com/']
+
+    user = 'excited-vczh'
+
+    user_url = 'https://www.zhihu.com/api/v4/members/{user}?include={include}'
+    user_query = 'allow_message,is_followed,is_following,is_org,is_blocking,employments,answer_count,follower_count,articles_count,gender,badge[?(type=best_answerer)].topics'
+
+    followers_url = 'https://www.zhihu.com/api/v4/members/{user}/followers?include={include}&offset={offset}&limit={limit}'
+    followers_query = 'data[*].answer_count,articles_count,gender,follower_count,is_followed,is_following,badge[?(type=best_answerer)].topics'
+
+    def start_requests(self):
+        yield Request(url=self.user_url.format(user=self.user, include=self.user_query), callback=self.parse_user)
+        yield Request(url=self.followers_url.format(user=self.user, include=self.followers_query, offset=0, limit=20),
+                      callback=self.parse_follows)
+
+    def parse_user(self, response):
+        result = json.loads(response.text)
+        item = UserItem()
+        for field in item.fields:
+            if field in result.keys():
+                item[field] = result[field]
+
+        yield item
+
+        # 获取每个用户的关注列表
+        yield Request(self.followers_url.format(user=item['url_token'], include=self.followers_query, offset=0, limit=20),
+                      callback=self.parse_follows)
+
+    def parse_follows(self, response):
+        results = json.loads(response.text)
+
+        if 'data' in results.keys():
+            for result in results.get('data'):
+                yield Request(self.user_url.format(user=result.get('url_token'), include=self.user_query),
+                              callback=self.parse_user)
+
+        if 'paging' in results.keys() and results.get('paging').get('is_end') == False:
+            next_page = results.get('paging').get('next')
+            yield Request(next_page, callback=self.parse_follows)
